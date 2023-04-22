ea49386b25c6715ee9a8129e199d93db6ada1dbd
Jonathan-Bush <114845816+Jonathan-Bush@users.noreply.github.com>
2023-04-22 00:38:03 -0400

    Updated minesweeper.js
    Added win condition, condensed functions, added easyChangeBoard()

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,70 @@
-var x=40,
-    y=24,
-    bc=(x*y)/5,
-    bombs=[],
-    flags=[];
+var x=28,//Board width (cells)
+    y=20,//Board height (cells)
+    bc=(x*y)/5,//Bomb count (recommended density between 1/2.4 and 1/10)
+    bombs=[],//initializing to empty
+    flags=[],//initializing to empty
+    colors = ['#1976d2','#3a8e3d','#d33433','#7b1fa2','#ff9100','darkturquoise','black','gray'];//colors for nums 1-8
 
-function createBoard() {
-    if(document.getElementById("board")) document.getElementById("board").remove()
+var varbs = document.querySelector(":root");//Get CSS variables
+window.addEventListener("resize",rsz,true);//Resize listener
+createBoard();//Start
+
+//Controls Functions
+function lc(c1,c2){
+    self = document.getElementById("x"+c1+"y"+c2); // Get Element
+    if(self.className == "b1"||self.className == "b2") return // Already-Clicked Check
+    if(bombs === undefined || bombs.length == 0){initialize(c1,c2); return} // Initialize Check
+    if(self.hasChildNodes() && self.firstChild.className=="flag") return // Stop click on flag
+    if(bombs[c1*y+c2]){self.style.backgroundColor = "red";lose(); return} // Click a Bomb Check    
+    self.className = (self.className == 'one' ? 'b1' : 'b2') // Apply Click
+    count = bombcheck(c1,c2)
+    if(count==0){ // Clear nearby tiles if blank
+        if(c1+1<x) lc(c1+1,c2)
+        if(c2+1<y) lc(c1,c2+1)
+        if(c1 > 0) lc(c1-1,c2)
+        if(c2 > 0) lc(c1,c2-1)
+        if(c1+1<x && c2+1<y) lc(c1+1,c2+1)
+        if(c1+1<x && c2 > 0) lc(c1+1,c2-1)
+        if(c1 > 0 && c2+1<y) lc(c1-1,c2+1)
+        if(c1 > 0 && c2 > 0) lc(c1-1,c2-1)
+    }
+    else if (count>0){ // Apply number to self if needed
+        self.innerHTML = count
+        self.style = "color:"+colors[count-1]
+    }
+    if((document.getElementsByClassName("one").length+document.getElementsByClassName("two").length)==bc) win();
+}//Applies left click (clear) to coordinates, includes most logic
+function mc(c1,c2){
+    self = document.getElementById("x"+c1+"y"+c2); // Get Element
+    if(self.className == "one"||self.className == "two") return // Must clear blanks
+    if(self.innerHTML && flagcheck(c1,c2) == bombcheck(c1,c2)){ //Allow on numbered tiles with equal numbers flags and bombs
+        if(c1+1<x) lc(c1+1,c2)
+        if(c2+1<y) lc(c1,c2+1)
+        if(c1 > 0) lc(c1-1,c2)
+        if(c2 > 0) lc(c1,c2-1)
+        if(c1+1<x && c2+1<y) lc(c1+1,c2+1)
+        if(c1+1<x && c2 > 0) lc(c1+1,c2-1)
+        if(c1 > 0 && c2+1<y) lc(c1-1,c2+1)
+        if(c1 > 0 && c2 > 0) lc(c1-1,c2-1)
+    }
+}//Applies middle click (chord) to coordinates
+function rc(c1,c2){
+    self = document.getElementById("x"+c1+"y"+c2); // Get Element
+    if(self.className == "b1"||self.className == "b2") return // Can't flag blanks
+    if(bombs === undefined || bombs.length == 0) return // Can't flag while pre-initialized
+    if(!self.innerHTML) {
+        self.innerHTML = "<img class=\"flag\" valign=\"middle\" align=\"center\" src=\"assets/flag.png\"></img>"
+        flags[c1*y+c2] = true
+    }
+    else {
+        self.innerHTML = ""
+        flags[c1*y+c2] = false
+        }
+}//Applies right click (flag) to coordinates
+
+//Mechanics Functions
+function createBoard(){
+    if(document.getElementById("board")) document.getElementById("board").remove()//drop previous board
     var board = $("<table id=\"board\" border=0 cellspacing=0>")
     for(var i=0;i<y;i++){
         row = "<tr>"
@@ -33,66 +92,10 @@ function createBoard() {
             }, false);
         }
     }
-}
-createBoard();
-var varbs = document.querySelector(":root");
-rsz();
-window.addEventListener("resize",rsz,true);
-
-//Controls Functions
-function lc(c1,c2){
-    self = document.getElementById("x"+c1+"y"+c2); // Get Element
-    if(self.className == "b1"||self.className == "b2") return // Already-Clicked Check
-    if(bombs === undefined || bombs.length == 0){initialize(c1,c2); return} // Initialize Check
-    if(self.hasChildNodes() && self.firstChild.className=="flag") return // Stop click on flag
-    if(bombs[c1*y+c2]){self.style.backgroundColor = "red";lose(); return} // Click a Bomb Check    
-    self.className = (self.className == 'one' ? 'b1' : 'b2') // Apply Click
-    count = bombcheck(c1,c2)
-    if(count==0){ // Clear nearby tiles if blank
-        if(c1+1<x) lc(c1+1,c2)
-        if(c2+1<y) lc(c1,c2+1)
-        if(c1 > 0) lc(c1-1,c2)
-        if(c2 > 0) lc(c1,c2-1)
-        if(c1+1<x && c2+1<y) lc(c1+1,c2+1)
-        if(c1+1<x && c2 > 0) lc(c1+1,c2-1)
-        if(c1 > 0 && c2+1<y) lc(c1-1,c2+1)
-        if(c1 > 0 && c2 > 0) lc(c1-1,c2-1)
-    }
-    else if (count>0){ // Apply number to self if needed
-        numerate(self,count)
-    }
-}
-function mc(c1,c2){
-    self = document.getElementById("x"+c1+"y"+c2); // Get Element
-    if(self.className == "one"||self.className == "two") return // Must clear blanks
-    if(self.innerHTML && flagcheck(c1,c2) == bombcheck(c1,c2)){ //Allow on numbered tiles with equal numbers flags and bombs
-        if(c1+1<x) lc(c1+1,c2)
-        if(c2+1<y) lc(c1,c2+1)
-        if(c1 > 0) lc(c1-1,c2)
-        if(c2 > 0) lc(c1,c2-1)
-        if(c1+1<x && c2+1<y) lc(c1+1,c2+1)
-        if(c1+1<x && c2 > 0) lc(c1+1,c2-1)
-        if(c1 > 0 && c2+1<y) lc(c1-1,c2+1)
-        if(c1 > 0 && c2 > 0) lc(c1-1,c2-1)
-    }
-}
-function rc(c1,c2){
-    self = document.getElementById("x"+c1+"y"+c2); // Get Element
-    if(self.className == "b1"||self.className == "b2") return // Can't flag blanks
-    if(bombs === undefined || bombs.length == 0) return // Can't flag while pre-initialized
-    if(!self.innerHTML) {
-        self.innerHTML = "<img class=\"flag\" valign=\"middle\" align=\"center\" src=\"assets/flag.png\"></img>"
-        flags[c1*y+c2] = true
-    }
-    else {
-        self.innerHTML = ""
-        flags[c1*y+c2] = false
-        }
-}
-
-//Mechanics Functions
+    rsz();bombs=[];flags=[];
+}//Generates table of y rows and x columns in a div of id="minesweeper" or appended to body
 function initialize(a,b){
-    b1 = []; flags = [];
+    b1 = [];
     SpaceReq = 20;
     if(bc/(x*y)>=(1./3.6)) SpaceReq = Math.min(15,(x*y)/10)
     else if(bc/(x*y)>=(1./5.0)) SpaceReq = Math.min(35,(x*y)/5)
@@ -108,7 +111,7 @@ function initialize(a,b){
     }
     if(!bombs[a*y+b]) lc(a,b);
     if((document.getElementsByClassName("b1").length+document.getElementsByClassName("b2").length)<SpaceReq) {createBoard();bombs=[];lc(a,b)}
-}
+}//Ran in lc() to ensure game has sufficient starting area
 function bombcheck(a,b){
     c = 0;
     if(a+1<x && bombs[(a+1)*y+b]) c++;//right
@@ -120,7 +123,7 @@ function bombcheck(a,b){
     if(a > 0 && b+1<y && bombs[(a-1)*y+(b+1)]) c++;//down left
     if(a > 0 && b > 0 && bombs[(a-1)*y+(b-1)]) c++;//up left
     return c;
-}
+}//Returns number of bombs around coords (a,b)
 function flagcheck(a,b){
     c = 0;
     if(a+1<x && flags[(a+1)*y+b]) c++;//right
@@ -132,20 +135,18 @@ function flagcheck(a,b){
     if(a > 0 && b+1<y && flags[(a-1)*y+(b+1)]) c++;//down left
     if(a > 0 && b > 0 && flags[(a-1)*y+(b-1)]) c++;//up left
     return c; 
-}
+}//Returns number of flags around coords (a,b)
 function lose(){
     alert('L you lost lol')
     for(i in bombs){//view bombs
         if(bombs[i] === true) document.getElementById("x"+Math.floor(i/y)+"y"+i%y).innerHTML = "<img class=\"mine\" valign=\"middle\" align=\"center\" src=\"assets/mine.png\"></img>";
     }
-    //createBoard()
-    //bombs = []
-}
-function numerate(elem,num){
-    colors = ['#1976d2','#3a8e3d','#d33433','#7b1fa2','#ff9100','darkturquoise','black','gray']
-    elem.innerHTML = num
-    elem.style = "color:"+colors[num-1]
-}
+    createBoard()
+}//Loss state function
+function win(){
+    alert('NO WAY YOU WON')
+    createBoard()
+}//Win state function
 
 //Display Functions
 function rsz(){
@@ -162,4 +163,15 @@ function rsz(){
     varbs.style.setProperty('--umargin',Math.ceil(margin)+'px')
     varbs.style.setProperty('--dmargin',Math.floor(margin)+'px')
     console.log("Page Resized: "+width+", "+height)
+}//Change CSS size variables based on page and table size
+
+//Admin Functions?
+function easyChangeBoard(w,h,d) {
+    if (w<8 || w>40) throw new Error('Width must be between 8 and 40 (default is 28)')
+    if (h<8 || h>24) throw new Error('Height must be between 8 and 24 (default is 20')
+    if (d<0.1 || d>0.4) throw new Error('Density must be between 0.1 and 0.4 (default is 0.2)')
+    x = w;
+    y = h;
+    bc = w*h*d;
+    createBoard();
 }
